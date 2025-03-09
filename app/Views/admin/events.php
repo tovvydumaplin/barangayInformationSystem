10e0d530c364f70f85dacda78a6f24ca439c6e63
Tovvy <tovvydumaplin@gmail.com>
2025-03-09 23:36:49 +0800

    updates on header

--- app/Views/admin/events.php
+++ app/Views/admin/events.php
@@ -35,6 +35,7 @@
   <body>
    <?= view ('includes/sidebar') ?>
     <main>
+    <?= view('includes/header.php') ?>
       <div class="wrapper"></div>
       <div id="createEventModal" class="modal">
         <div class="modal__header">
@@ -178,79 +179,6 @@
           </div>
         </form>
       </div>
-
-      <header class="header">
-        <div class="section__title__container">
-          <div class="menu__icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="ionicon"
-              viewBox="0 0 512 512"
-            >
-              <path
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-miterlimit="10"
-                stroke-width="32"
-                d="M80 160h352M80 256h352M80 352h352"
-              />
-            </svg>
-          </div>
-          <h2 class="heading__secondary">Events</h2>
-        </div>
-        <div class="user__box">
-          <img
-            src="circle.png"
-            class="user__image"
-            alt="profile image of user"
-          />
-          <p class="user__name">John Doe</p>
-          <ion-icon class="chev__down" name="chevron-down-outline"></ion-icon>
-          <div class="dropdown__menu hide">
-            <p class="user__name__menu">Welcome, <span>John!</span></p>
-            <hr />
-            <a href="#" class="link">
-              <div class="icon__link">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class=""
-                  viewBox="0 0 512 512"
-                >
-                  <path
-                    d="M262.29 192.31a64 64 0 1057.4 57.4 64.13 64.13 0 00-57.4-57.4zM416.39 256a154.34 154.34 0 01-1.53 20.79l45.21 35.46a10.81 10.81 0 012.45 13.75l-42.77 74a10.81 10.81 0 01-13.14 4.59l-44.9-18.08a16.11 16.11 0 00-15.17 1.75A164.48 164.48 0 01325 400.8a15.94 15.94 0 00-8.82 12.14l-6.73 47.89a11.08 11.08 0 01-10.68 9.17h-85.54a11.11 11.11 0 01-10.69-8.87l-6.72-47.82a16.07 16.07 0 00-9-12.22 155.3 155.3 0 01-21.46-12.57 16 16 0 00-15.11-1.71l-44.89 18.07a10.81 10.81 0 01-13.14-4.58l-42.77-74a10.8 10.8 0 012.45-13.75l38.21-30a16.05 16.05 0 006-14.08c-.36-4.17-.58-8.33-.58-12.5s.21-8.27.58-12.35a16 16 0 00-6.07-13.94l-38.19-30A10.81 10.81 0 0149.48 186l42.77-74a10.81 10.81 0 0113.14-4.59l44.9 18.08a16.11 16.11 0 0015.17-1.75A164.48 164.48 0 01187 111.2a15.94 15.94 0 008.82-12.14l6.73-47.89A11.08 11.08 0 01213.23 42h85.54a11.11 11.11 0 0110.69 8.87l6.72 47.82a16.07 16.07 0 009 12.22 155.3 155.3 0 0121.46 12.57 16 16 0 0015.11 1.71l44.89-18.07a10.81 10.81 0 0113.14 4.58l42.77 74a10.8 10.8 0 01-2.45 13.75l-38.21 30a16.05 16.05 0 00-6.05 14.08c.33 4.14.55 8.3.55 12.47z"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="32"
-                  />
-                </svg>
-              </div>
-              Account Settings</a
-            >
-            <a href="#" class="link">
-              <div class="icon__link">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="ionicon"
-                  viewBox="0 0 512 512"
-                >
-                  <path
-                    d="M304 336v40a40 40 0 01-40 40H104a40 40 0 01-40-40V136a40 40 0 0140-40h152c22.09 0 48 17.91 48 40v40M368 336l80-80-80-80M176 256h256"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="32"
-                  />
-                </svg>
-              </div>
-              Logout</a
-            >
-          </div>
-        </div>
-      </header>
       <div class="container">
         <div class="card">
           <div class="heading__container">
