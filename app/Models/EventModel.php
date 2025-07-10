dc608cc2cde023935ec3e709283082aabdf4de88
Tovvy Dumalin <tovvydumaplin@gmail.com>
2025-07-10 23:00:59 +0800

    Update EventModel.php

--- app/Models/EventModel.php
+++ app/Models/EventModel.php
@@ -7,7 +7,7 @@ class EventModel extends Model
   protected $table = 'tbl_event';
   protected $primaryKey = 'event_id';
   protected $allowedFields = ['event_id','event_title','event_description','start_date','end_date','status','created_at','updated_at'];
-  protected $useTimestamps = true;  // Enable automatic timestamps
+  protected $useTimestamps = true;  
   protected $createdField  = 'created_at';
   protected $updatedField  = 'updated_at';
 }
